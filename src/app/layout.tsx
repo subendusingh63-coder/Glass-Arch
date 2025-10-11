c5d028bc97e0d0c434ec052561005f3c4636d83e
subendusingh63-coder <subendusingh63@gmail.com>
2025-10-11 10:57:49 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-import '../globals.css'
-import Footer from '@/components/footer'
-import Navbar from '@/components/navbar'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "../components/footer";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Glass Arch',
-  description: 'Arching Ideas into Reality'
-}
+  title: "Glass Arch",
+  description: "Modern Glass Architecture Website by We2Variety",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
+      <body className={inter.className}>
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
