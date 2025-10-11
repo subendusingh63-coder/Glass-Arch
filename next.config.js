1ac6730717817985515fb47c7366c9dd09400a72
subendusingh63-coder <subendusingh63@gmail.com>
2025-10-11 10:55:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
-    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+    allowedDeVOrigins: ["https://*.cloudworkstations.dev"],
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -14,37 +12,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.postimg.cc',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
